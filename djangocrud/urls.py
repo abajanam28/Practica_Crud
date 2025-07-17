aec6d005a729532087443c13ed0463e5153ca4e6
Andres Bajaña <andresbajana2@gmail.com>
2025-07-16 21:48:02 -0500

    Proyecto crud terminado

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -22,7 +22,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home' ),
     path('signup/', views.signup, name='signup' ),
-    path('signin/', views.signin, name='signin' ),
     path('tasks/', views.tasks, name='tasks' ),
+    path('tasks_completed', views.tasks_completed, name='tasks_completed' ),
+    path('tasks/create/', views.create_task, name='create_task'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_tasks, name='delete_tasks'),
     path('logout/', views.signout, name='logout' ),
+    path('signin/', views.signin, name='signin' ),
 ]
